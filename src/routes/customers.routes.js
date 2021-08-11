ea06fde67adfe17c514cc3e7fc60073f16580266
Alexis Guanche <silver.mugiwara@gmail.com>
2021-08-12 00:21:46 +0100

    Created middlewares for authentication and authorization

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -2,10 +2,11 @@ import { Router } from 'express'
 const router = Router()
 
 import * as customersCtrl from '../controllers/customers.controller'
+import { authJWT } from '../middlewares'
 
 router.post('/', customersCtrl.createCustomer)
-router.get('/', customersCtrl.getCustomers)
-router.get('/:customerId', customersCtrl.getCustomerById)
+router.get('/', authJWT.verifyToken, customersCtrl.getCustomers)
+router.get('/:customerId', authJWT.verifyToken, authJWT.isAdmin, customersCtrl.getCustomerById)
 router.put('/:customerId', customersCtrl.updateCustomerById)
 router.delete('/:customerId', customersCtrl.deleteCustomerById)
 
