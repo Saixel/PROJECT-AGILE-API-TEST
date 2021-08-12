e628a21f0d3050b808312fdec4400ccd06bce5cc
Alexis Guanche <silver.mugiwara@gmail.com>
2021-08-12 21:12:15 +0100

    Ready for the first deploy test

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ config()
 
 export default {
   PORT: process.env.PORT || 3000,
-  SECRET: process.env.SECRET || 'customers-api-secret',
+  SECRET: process.env.SECRET || 'your-ultra-secret-here',
   MONGO_URL: process.env.MONGO_URL || 'mongodb://localhost',
   MONGO_DB: process.env.MONGO_DB || 'agile-test',
 }
